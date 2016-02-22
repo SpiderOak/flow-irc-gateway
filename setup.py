1b5834ca6bc91f20a38821372c4daf716df89c5f
Lucas Manuel Rodriguez <lucarodriguez@gmail.com>
2016-02-22 19:28:57 -0300

    Amend version

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(name = "Flow-IRC-Gateway",
-      version = "1.0",
+      version = "0.1",
       package_dir = { "": "src" },
       py_modules = [ "common", "irc_client", "channel", "flow.flow_api", "notification" ],
       scripts = [ "src/flow-irc-gateway" ], 
       install_requires = [ "requests" ],
-      keywords = [ "spideroak", "irc", "flow", "gateway" ],
+      keywords = [ "spideroak", "irc", "flow", "gateway", "semaphor" ],
       author = "Lucas Manuel Rodriguez",
-      author_email = "lucarodriguez@gmail.com",
+      author_email = "lucas@spideroak-inc.com",
       description = "flow-irc-gateway is a small IRC gateway to SpiderOak Flow (based off of miniircd)",
 )
